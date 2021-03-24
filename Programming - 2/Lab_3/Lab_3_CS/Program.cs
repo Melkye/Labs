70479fd74e8635f572fa414159525baaa28beab2
Denys <melkye777@gmail.com>
2021-03-24 20:44:39 +0200

    Lab 3 Prog

---
+++ Programming - 2/Lab_3/Lab_3_CS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Symbols;
+
+namespace Lab_3_CS
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] symbols = {'s', 'o', 'm', 'e', 'k', 'i', 'n', 'd',
+                'o', 'f', 'w', 'o', 'r', 'd', 's'};
+            CharArray theWord = new CharArray(symbols);
+            int nVowels = theWord.NumberOfVowels;
+            char first = theWord[0];
+            theWord[0] = 'a';
+            nVowels = theWord.NumberOfVowels;
+            theWord[22] = '5';
+        }
+    }
+}
---
+++ Programming - 2/Lab_3/Symbols/CharArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Symbols
+{
+    public class CharArray
+    {
+        public CharArray(char[] data)
+        {
+            Data = data;
+        }
+        public char[] Data { get; set; }
+        public char this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > Data.Length - 1)
+                    throw new ArgumentOutOfRangeException();
+                return Char.ToUpper(Data[index]);
+            }
+            set
+            {
+                if (index < 0 || index > Data.Length - 1)
+                    throw new ArgumentOutOfRangeException();
+                Data[index] = value;
+            }
+        }
+
+        public int NumberOfVowels
+        {
+            get
+            {
+                int numberOfVowels = 0;
+                for (int i = 0; i < Data.Length; i++)
+                {
+                    if (Data[i] == 'a' || Data[i] == 'o' || Data[i] == 'u' ||
+                        Data[i] == 'e' || Data[i] == 'i')
+                        numberOfVowels++;
+                }
+                return numberOfVowels;
+            }
+        }
+    }
+}
