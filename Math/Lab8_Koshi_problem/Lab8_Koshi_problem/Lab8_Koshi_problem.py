128ba451faf27eb1f61acc56a88880439c9856fc
Denys <melkye777@gmail.com>
2021-05-19 22:57:57 +0300

    Lab_8 Math

--- Math/Lab8_Koshi_problem/Lab8_Koshi_problem/Lab8_Koshi_problem.py
+++ Math/Lab8_Koshi_problem/Lab8_Koshi_problem/Lab8_Koshi_problem.py
@@ -1,4 +1,6 @@
+#Lab8_Koshi_problem.py
 import methods
+import matplotlib.pyplot as plt
 
 def func(x, y):
     return 1 + 1.8*y*methods.np.sin(x) - y**2
@@ -8,5 +10,29 @@ y0 = 0
 x_end = 6
 h = 0.1
 
-x1, y1, e1 = methods.runge_kutta(x0, y0, x_end, func, h) # round x when writing to console
-a = 1
+x1, y1, h = methods.runge_kutta(x0, y0, x_end, func, h, True)
+x1_halved, y1_halved = methods.runge_kutta(x0, y0, x_end, func, h/2, False)
+
+x2, y2 = methods.adams(x1[:4], y1[:4], x_end, func, h)
+x2_halved, y2_halved = methods.adams(x1_halved[:4], y1_halved[:4], x_end, func, h/2)
+
+e1 = methods.evaluate_error_runge(y1, y1_halved, 4)
+e2 = methods.evaluate_error_runge(y2, y2_halved, 4)
+
+print(f"   N x        Runge-Kutta  e1         Adams       e2")
+for i in range(len(x1)):
+    print(f" {i:>3} {round(x1[i], 5):<6}   {y1[i]:8.6f}    {e1[i]:8.1e}    {y2[i]:8.6f}   {e2[i]:8.1e}")
+
+fig = plt.gcf() # to be able to change window title
+fig.canvas.set_window_title("Розв'язок")
+plt.plot(x1, y1, 'b', label = "Метод Рунге-Кутта")
+plt.plot(x2, y2, 'y', label = "Метод Адамса")
+plt.legend(loc="best")
+plt.show()
+
+fig = plt.gcf() # to be able to change window title
+fig.canvas.set_window_title("Похибка")
+plt.plot(x1, e1, 'b', label = "Похибка методу Рунге-Кутта")
+plt.plot(x2, e2, 'y', label = "Похибка методу Адамса")
+plt.legend(loc="best")
+plt.show()
--- Math/Lab8_Koshi_problem/Lab8_Koshi_problem/methods.py
+++ Math/Lab8_Koshi_problem/Lab8_Koshi_problem/methods.py
@@ -1,72 +1,11 @@
+#methods.py
 import numpy as np
 
-def func(x, y):
-    return 1 + 1.8*y*np.sin(x) - y**2
-
-def runge_kutta(x0, y0, x_end, func, h):
-
-    ## for step h
-    #x = [x0]
-    #y = [y0]
-    #n = int((x_end - x0)/h + 1)  # +1 to include x0, int to use as index
-    #for i in range(n + 1):  # +1 to include the end
-    #    K1 = func(x[i], y[i])
-    #    K2 = func(x[i] + h/2, y[i] + h*K1/2)
-    #    K3 = func(x[i] + h/2, y[i] + h*K2/2)
-    #    K4 = func(x[i] + h, y[i] + h*K3)
-
-    #    #    h /= 2
-    #    #    K1 = func(x[i], y[i])
-    #    #    K2 = func(x[i] + h/2, y[i] + h*K1/2)
-    #    #    K3 = func(x[i] + h/2, y[i] + h*K2/2)
-    #    #    K4 = func(x[i] + h, y[i] + h*K3)
-    #    delta_y = (h/6)*(K1 + 2*K2 + 2*K3 + K4)
-    #    y.append(y[i] + delta_y)
-    #    x.append(x[i] + h)
-    #    if np.abs((K2-K3)/(K1-K2)) > 0.025:
-    #        print("shit")
-    #        x = [x0]
-    #        y = [y0]
-    #        h /= 2
-    #        n = int((x_end - x0)/h + 1)
-    #        i = 0
-    x1, y1, h = runge_kutta_iteration(x0, y0, x_end, func, h, 1)
-    # for step h/2 - to evaluate error
-    x2 = [x0]
-    y2 = [y0]
-    n2 = int((x_end - x0)/(h/2)) + 1  # +1 to include x0
-    #for i in range(n2 + 1):  # +1 to include the end, int to use as index
-    #    K1 = func(x2[i], y2[i])
-    #    K2 = func(x2[i] + h/2, y2[i] + h*K1/2)
-    #    K3 = func(x2[i] + h/2, y2[i] + h*K2/2)
-    #    K4 = func(x2[i] + h, y2[i] + h*K3)
-    #    #while np.abs((K2-K3)/(K1-K2)) > 0.025:
-    #    #    h /= 2
-    #    #    K1 = func(x[i], y[i])
-    #    #    K2 = func(x[i] + h/2, y[i] + h*K1/2)
-    #    #    K3 = func(x[i] + h/2, y[i] + h*K2/2)
-    #    #    K4 = func(x[i] + h, y[i] + h*K3)
-    #    delta_y = (h/6)*(K1 + 2*K2 + 2*K3 + K4)
-    #    y2.append(y2[i] + delta_y)
-    #    x2.append(x2[i] + h)
-    x2, y2 = runge_kutta_iteration(x0, y0, x_end, func, h/2, 2)
-
-    #evaluating error
-    e = [0]
-    for i, j in zip(range(len(x1) + 1), range(0, len(x2) + 1, 2)):
-        error_i = np.abs(y1[i] - y2[j])/15
-        e.append(error_i)
-        print(str(e))
-    return x1, y1, e
-
-#def adams(x0, y0, h,)
-
-def runge_kutta_iteration(x0, y0, x_end, func, h, step): # necessary to check for '> 0.025' condition only
-        # for step h                                     # on during the iteration with step h
+def runge_kutta(x0, y0, x_end, func, h, eval_theta):
     x = [x0]
     y = [y0]
-    n = int((x_end - x0)/h + 1)  # +1 to include x0, int to use as index
-    for i in range(n + 1):  # +1 to include the end
+    n_sections = int((x_end - x[0])/h)  # int to use as index
+    for i in range(n_sections):
         K1 = func(x[i], y[i])
         K2 = func(x[i] + h/2, y[i] + h*K1/2)
         K3 = func(x[i] + h/2, y[i] + h*K2/2)
@@ -75,13 +14,44 @@ def runge_kutta_iteration(x0, y0, x_end, func, h, step): # necessary to check fo
         delta_y = (h/6)*(K1 + 2*K2 + 2*K3 + K4)
         y.append(y[i] + delta_y)
         x.append(x[i] + h)
-        if step == 1:
-            if np.abs((K2-K3)/(K1-K2)) > 0.025:
-                print("shit", str(h), str(y[i]), str(np.abs((K2-K3)/(K1-K2))))
-                x, y, h = runge_kutta_iteration(x0, y0, x_end, func, h/2, 1)
-                break
-    if step == 1:
+        #if eval_theta:
+        #    if np.abs((K2-K3)/(K1-K2)) > 0.025:
+        #        print(f"splitting h = {h:6}, in half, because theta = {np.abs((K2-K3)/(K1-K2)):.2}")
+        #        x, y, h = runge_kutta(x0, y0, x_end, func, h/2, True)
+        #        break
+    if eval_theta:
         return x, y, h
     else:
         return x, y
-            
+
+
+def adams(x0, y0, x_end, func, h):
+    x = x0[:]                                    
+    y = y0[:]
+    are_results_different = True
+    n_sections = int((x_end - x[0])/h)
+
+    for i in range(3, n_sections, 1):
+        x.append(x[i] + h)
+        delta_y = (h/24)*(55*func(x[i], y[i]) - 59*func(x[i-1], y[i-1]) \
+            + 37*func(x[i-2], y[i-2]) - 9*func(x[i-3], y[i-3]))
+        y_ex = y[i] + delta_y
+
+        y.append(y_ex)
+        if are_results_different:
+            delta_y = (h/24)*(9*func(x[i+1], y_ex) + 19*func(x[i], y[i]) \
+            - 5*func(x[i-1], y[i-1]) + func(x[i-2], y[i-2]))
+            y_in = y[i] + delta_y
+            y.pop()
+            y.append(y_in)
+
+        if np.abs(y_ex - y_in) <= 0.0000001:
+            are_results_different = False
+    return x, y
+
+def evaluate_error_runge(y_h, y_halved_h, error_order):
+    e = []
+    for i, j in zip(range(len(y_h) + 1), range(0, len(y_halved_h) + 1, 2)):
+        error_i = np.abs(y_h[i] - y_halved_h[j])/(2**error_order - 1)
+        e.append(error_i)
+    return e
