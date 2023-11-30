3fc969e2dd31d91df6df90fc0df43b08d02a6872
Denys Adamov <melkye777@gmail.com>
2023-11-30 12:09:26 +0200

    test and fix majoritar

--- DecisionTheory/practice_3/app/practice_3.py
+++ DecisionTheory/practice_3/app/practice_3.py
@@ -41,6 +41,8 @@ def get_sigma_matrix(alternatives_ratings_by_criteria):
                 alternatives_ratings_by_criteria[x], alternatives_ratings_by_criteria[y])
             sigma_matrix[x].append(sigma)
 
+    return sigma_matrix
+
 # ---
 
 
@@ -106,7 +108,6 @@ def get_lexicographic_relation(alternatives_ratings_by_criteria, criteria_import
 
             is_added = False
             for i in range(len(sigma_matrix_ordered_by_criteria_desc_importance[x][y])):
-
                 if sigma_matrix_ordered_by_criteria_desc_importance[x][y][i] > 0:
                     lexicographic_relation[x].append(1)
                     is_added = True
@@ -117,7 +118,93 @@ def get_lexicographic_relation(alternatives_ratings_by_criteria, criteria_import
                     lexicographic_relation[x].append(0)
                     is_added = True
                     break
-            if is_added:
-                break
+            if not is_added:
+                lexicographic_relation[x].append(0)
 
     return lexicographic_relation
+
+
+def print_relation(relation):
+    for row in relation:
+        for element in row:
+            if isinstance(element, int) or isinstance(element, float):
+                print_to_file("{:.0f}".format(element) + "  ", end="")
+            else:
+                print_to_file(element + "  ", end="")
+        print_to_file()
+
+
+def print_to_file(data="", end="\n"):
+    with open("Var-01-АдамовДенис.txt", "a") as file:
+        file.write(str(data))
+        file.write(end)
+
+
+def main():
+    pareto_relation = get_pareto_relation(alternatives_ratings_by_criteria)
+    majoritar_relation = get_majoritar_relation(
+        alternatives_ratings_by_criteria)
+    lexicographic_relation = get_lexicographic_relation(
+        alternatives_ratings_by_criteria, criteria_importance_desc)
+
+    with open("Var-01-АдамовДенис.txt", "w") as file:
+        file.truncate()
+    print_to_file(1)
+    print_relation(pareto_relation)
+    print_to_file(2)
+    print_relation(majoritar_relation)
+    print_to_file(3)
+    print_relation(lexicographic_relation)
+# ---
+
+
+alternatives_ratings_by_criteria = [
+    [8, 8, 10, 1, 3, 2, 1, 9, 4, 6, 5, 9],
+    [8, 6, 9, 1, 3, 2, 1, 6, 4, 1, 5, 6],
+    [6, 4, 4, 1, 3, 2, 1, 4, 4, 1, 5, 5],
+    [5, 4, 4, 1, 2, 2, 1, 2, 4, 1, 4, 5],
+    [8, 8, 10, 9, 8, 6, 2, 9, 4, 10, 5, 9],
+    [8, 7, 7, 8, 6, 5, 2, 4, 2, 5, 2, 9],
+    [2, 4, 6, 4, 2, 5, 2, 3, 1, 5, 2, 9],
+    [2, 4, 6, 4, 2, 4, 2, 3, 1, 4, 2, 8],
+    [6, 10, 6, 6, 7, 5, 3, 10, 6, 5, 10, 9],
+    [10, 10, 6, 6, 10, 8, 3, 10, 6, 6, 10, 9],
+    [2, 1, 6, 4, 2, 2, 2, 3, 1, 4, 2, 8],
+    [5, 2, 10, 4, 3, 2, 8, 6, 4, 4, 10, 8],
+    [5, 2, 6, 4, 1, 2, 1, 6, 4, 2, 7, 3],
+    [9, 2, 6, 4, 1, 6, 5, 8, 4, 10, 7, 3],
+    [9, 6, 9, 4, 9, 6, 8, 8, 4, 10, 7, 9],
+    [9, 6, 9, 4, 9, 6, 8, 9, 4, 10, 7, 9],
+    [8, 3, 2, 1, 6, 6, 8, 3, 4, 10, 7, 9],
+    [8, 1, 2, 1, 6, 4, 8, 3, 1, 2, 3, 4],
+    [8, 10, 6, 9, 7, 5, 8, 10, 6, 7, 10, 9],
+    [10, 10, 6, 9, 7, 10, 8, 10, 8, 7, 10, 9]
+]
+
+criteria_importance_desc = [8, 10, 3, 0, 4, 5, 11, 2, 1, 7, 9, 6]
+
+# alternatives_ratings_by_criteria = [
+#     [4, 6, 9,  4, 6,  5,  7, 10,  6,  8,  8,  9],
+#     [4,  6,  9,  4,  6,  6,  7, 10,  6,  9,  8,  9],
+#     [2,  6,  9,  4,  6,  5,  6, 10,  1,  5,  8,  6],
+#     [2,  8, 10,  9,  6,  8,  6, 10,  7, 10,  8,  8],
+#     [10,  8, 10,  9,  7,  8,  6, 10,  7, 10,  8,  8],
+#     [6,  6,  3,  6,  7,  1,  3,  5,  7,  5,  2,  5],
+#     [2,  6,  2,  6,  6,  1,  2,  5,  6,  1,  2,  5],
+#     [2,  2,  2,  6,  2,  1,  2,  4,  6,  1,  2,  2],
+#     [6,  8,  4,  8,  7,  3,  5,  5,  7,  6,  5,  8],
+#     [6,  4,  4,  7,  3,  3,  5,  1,  3,  5,  3,  2],
+#     [7,  6,  9, 10,  6,  5,  7, 10,  6,  8,  8,  9],
+#     [7,  6,  9, 10,  9,  5,  7, 10, 10,  8,  8,  9],
+#     [6,  2,  4,  2,  3,  3,  1,  1,  2,  4,  3,  2],
+#     [6,  1,  3,  2,  3,  3,  1,  1,  2,  4,  3,  2],
+#     [6,  1,  3,  2,  3,  1,  1,  1,  2,  4,  2,  2],
+#     [6,  1,  3,  2,  3,  1,  1,  1,  2,  2,  2,  1],
+#     [10, 10,  6,  6,  4,  7,  7,  5,  8,  8, 9,  5],
+#     [6,  4,  6,  5,  4,  1,  1,  5,  3,  8,  9,  5],
+#     [6,  4,  4,  3,  3,  1, 1,  1,  3,  5,  3,  2],
+#     [6, 10,  8,  9,  3,  3,  1,  8,  3,  7,  9,  6]
+# ]
+# criteria_importance_desc = [6, 4, 10, 2, 11, 3, 8, 12, 1, 5, 9, 7]
+
+main()
